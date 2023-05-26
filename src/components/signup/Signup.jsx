2f54020f148e82b0f7497e9c37bba265fe5024f6
Diegantos <diegantos8@gmail.com>
2023-05-26 14:59:43 +0200

    basic form

--- src/components/signup/Signup.jsx
+++ src/components/signup/Signup.jsx
@@ -1,23 +1,26 @@
-import './Signup.scss'
+import "./Signup.scss";
 
 export const Signup = () => {
-    return(
-        <div className="Login">
-            <img src="./assets/logo.svg" alt="" />
-            <h2>Sign up</h2>
-            <form action="">
-                <div className="Username">
-                    <input 
-                        type="text"
-                        className='Username-input'
-                        placeholder='Username' />
-                    <div className="Spinner"></div>
-                </div>
-                <input 
-                    type="password" 
-                    name="password"
-                    placeholder='Password' />
-            </form>
+  return (
+    <div className="Login">
+      <img src="./assets/logo.svg" alt="" />
+      <h2>Sign up</h2>
+
+      <form action="">
+        <div className="Username">
+          <input
+            type="text"
+            className="Username-input"
+            placeholder="Username"
+          />
+          <div className="Spinner"></div>
         </div>
-    )
-}
+        <input type="password" name="password" placeholder="Password" />
+
+        <div className="Bars"></div>
+        <div className="Strength"></div>
+        <button className="Login-button">JOIN NOW</button>
+      </form>
+    </div>
+  );
+};
